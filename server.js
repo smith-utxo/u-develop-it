1e436b11d181886e22aa512083e9da0b213a684b
Adam Smith <smith.adam937@gmail.com>
2022-07-12 10:08:44 -0500

    add CRUD routes

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const PORT = process.env.PORT || 3001; 
 const app = express(); 
 const mysql = require('mysql2');
+const inputCheck = require('./utils/inputCheck');
 
 // Express Middleware
 // express.urlencoded function is a built-in middleware function Express. It parses incoming requests with urlencoded payloads. 
@@ -35,11 +36,87 @@ app.get('/', (req, res) => {
 db.query(`SELECT * FROM candidates`, (err, rows) => {
   console.log(rows);
 })
+
+// Get all candidates
+app.get('/api/candidates', (req, res) => {
+  const sql = `SELECT * FROM candidates`;
+
+  db.query(sql, (err, rows) => {
+    if (err) {
+      res.status(500).json({ error: err.message });
+      return;
+    }
+    //If there is no error than err is null and the response is sent back using the following statement. Not that the response is as a JSON object to the browser using res in the express.js route callback: 
+    res.json({
+      message: 'success',
+      data: rows
+    });
+  });
+});
+
+// Delete a candidate
+app.delete('/api/candidate/:id', (req, res) =>{
+  const sql = `DELETE FROM candidates WHERE id = ?`;
+  const params = [req.params.id];
+
+  db.query(sql, params, (err, result) => {
+    if(err){
+      res.statusMessage(400).json({error: res.message});
+    } else if (!result.affectedRows) {
+      res.json({
+        message: 'Candidate not found'
+      })
+    }else{
+      res.json({
+        message: 'deleted',
+        changes: result.affectedRows,
+        id: req.params.id
+      })
+    }
+  })
+
+})
+
+// Create a candidate
+app.post('/api/candidate', ({ body }, res) => {
+  const errors = inputCheck(body, 'first_name', 'last_name', 'industry_connected');
+  if (errors) {
+    res.status(400).json({ error: errors });
+    return;
+  }
+  const sql = `INSERT INTO candidates (first_name, last_name, industry_connected)
+  VALUES (?,?,?)`;
+const params = [body.first_name, body.last_name, body.industry_connected];
+
+db.query(sql, params, (err, result) => {
+  if (err) {
+    res.status(400).json({ error: err.message });
+    return;
+  }
+  res.json({
+    message: 'success',
+    data: body
+  });
+});
+});
+
 // Create catchall route to handle user requests and aren't supported by the app. 
 //SUPER IMPORTANT TO PLACE THIS BELOW THE app.get or it will override them and you will get the error for correct routes as well!
+
+// Create a candidate
+/* const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected) 
+              VALUES (?,?,?,?)`; // Placeholders
+const params = [1, 'Ronald', 'Firbank', 1]; // declared a variable for parameters for legibility for the call function to the database. ORDER HERE MUST MATCH WHAT'S IN PARENTHESIS ABOVE. ALSO MUST MATCH THE NUMBER OF ? PLACEHOLDERS
+
+db.query(sql, params, (err, result) => {
+  if (err) {
+    console.log(err);
+  }
+  console.log(result);
+});
 app.use((req, res) => {
   res.status(404).end();
-})
+}) */
 
 // listen to PORT 
 app.listen(PORT, () => {
