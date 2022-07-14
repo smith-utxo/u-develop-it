b802da6c0f18bf2386b3d5ad0cfddcdabcf5e377
Adam Smith <smith.adam937@gmail.com>
2022-07-14 11:32:21 -0500

    server.js get routes updated with join statements

--- server.js
+++ server.js
@@ -39,7 +39,11 @@ db.query(`SELECT * FROM candidates`, (err, rows) => {
 
 // Get all candidates
 app.get('/api/candidates', (req, res) => {
-  const sql = `SELECT * FROM candidates`;
+  const sql = `SELECT candidates.*, parties.name
+    AS party_name 
+    FROM candidates 
+    LEFT JOIN parties 
+    ON candidates.party_id = parties.id`;
 
   db.query(sql, (err, rows) => {
     if (err) {
@@ -56,7 +60,12 @@ app.get('/api/candidates', (req, res) => {
 
 // Delete a candidate
 app.delete('/api/candidate/:id', (req, res) =>{
-  const sql = `DELETE FROM candidates WHERE id = ?`;
+  const sql = `SELECT candidates.*, parties.name 
+             AS party_name 
+             FROM candidates 
+             LEFT JOIN parties 
+             ON candidates.party_id = parties.id 
+             WHERE candidates.id = ?`;
   const params = [req.params.id];
 
   db.query(sql, params, (err, result) => {
